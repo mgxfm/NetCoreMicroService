597a07c1ecf44fe9c60c5c17b622c98b38eccad7
mark <837216547@qq.com>
2019-06-13 18:28:32 +0800

    上传10节示例代码

---
+++ 10/IdentityServerSample/IdentityServerCenter/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace IdentityServerCenter
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()                              //依赖注入添加IndentityServer
+                    .AddDeveloperSigningCredential()                  //证书加密配置，可以查看下AddDeveloperSigningCredential实现源码，启动项目，
+                                                                      //IdentityServer4 会在项目目录中生成一个tempkey.rsa证书文件
+                    .AddInMemoryApiResources(Config.GetApiResource()) //加入内存级别的ApiResource
+                    .AddInMemoryClients(Config.GetClient());          //加入内存级别的Client
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseIdentityServer();
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
---
+++ 10/IdentityServerSample/ThirdPartyDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using IdentityModel.Client;
+using static IdentityModel.OidcConstants;
+
+namespace ThirdPartyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1、根据entpoint获取token
+            var diso = DiscoveryClient.GetAsync("http://localhost:5000").Result;
+            if(diso.IsError)
+            {
+                Console.WriteLine(diso.Error);
+            }
+            var tokenClient = new TokenClient(diso.TokenEndpoint,"client","secret");
+            var tokenResponse = tokenClient.RequestClientCredentialsAsync("api").Result;
+            if(tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+            }
+            else
+            {
+                Console.WriteLine(tokenResponse.Json);
+            }
+            var httpClient = new HttpClient();
+            httpClient.SetBearerToken(tokenResponse.AccessToken);
+            var response = httpClient.GetAsync("http://localhost:5001/api/values").Result;
+            Console.WriteLine(response.Content);
+
+            Console.ReadLine();
+        }
+    }
+}
