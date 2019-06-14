af90cdce1eb9c4f612f61a0c288839db1413d4c9
mark <837216547@qq.com>
2019-06-14 15:34:03 +0800

    上传15节代码

---
+++ Part1-IdentityServer4/15/authSample/JwtAuthSample/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace JwtAuthSample.Controllers
+{
+
+    [Authorize(Policy="SuperAdminOnly")]
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        /// <summary>
+        /// 获取值
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<string> Get()
+        {
+            return User.Claims.Select(c=> c.Type +" " + c.Value ).ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        ///Summary 
+        
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
