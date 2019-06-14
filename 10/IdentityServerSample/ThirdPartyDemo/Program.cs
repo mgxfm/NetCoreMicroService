11e5f2265e7c721301af2095bc431bed813e5e0a
mark <837216547@qq.com>
2019-06-14 09:00:07 +0800

    加入第三方调用ClentCredentail注释

--- 10/IdentityServerSample/ThirdPartyDemo/Program.cs
+++ 10/IdentityServerSample/ThirdPartyDemo/Program.cs
@@ -9,12 +9,13 @@ namespace ThirdPartyDemo
     {
         static void Main(string[] args)
         {
-            //1、根据entpoint获取token
+            // 1、向授权服务器获取Token
             var diso = DiscoveryClient.GetAsync("http://localhost:5000").Result;
             if(diso.IsError)
             {
                 Console.WriteLine(diso.Error);
             }
+            // 1-1、根据entpoint获取token
             var tokenClient = new TokenClient(diso.TokenEndpoint,"client","secret");
             var tokenResponse = tokenClient.RequestClientCredentialsAsync("api").Result;
             if(tokenResponse.IsError)
@@ -27,6 +28,7 @@ namespace ThirdPartyDemo
             }
             var httpClient = new HttpClient();
             httpClient.SetBearerToken(tokenResponse.AccessToken);
+            //2、携带token访问资源api
             var response = httpClient.GetAsync("http://localhost:5001/api/values").Result;
             Console.WriteLine(response.Content);
 
