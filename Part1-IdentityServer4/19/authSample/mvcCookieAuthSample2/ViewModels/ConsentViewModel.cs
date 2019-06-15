b85581ed0aad2bcddbbcecc8608ceec9536793a0
mark <837216547@qq.com>
2019-06-15 11:43:35 +0800

    上传19节示例代码-Consent视图制作

---
+++ Part1-IdentityServer4/19/authSample/mvcCookieAuthSample2/ViewModels/ConsentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcCookieAuthSample.ViewModels
+{
+    public class ConsentViewModel
+    {
+        public string ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string ClientLogoUrl { get; set; }
+        public string AllowRememberConsent { get; set; }
+        public IEnumerable<ScopesViewModel> IdentityScopes { get; set; }
+        public IEnumerable<ScopesViewModel> ResourceScopes { get; set; }
+    }
+}
---
+++ Part1-IdentityServer4/19/authSample/mvcCookieAuthSample2/ViewModels/ScopesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcCookieAuthSample.ViewModels
+{
+    public class ScopesViewModel
+    {
+        public string Name { get; set; }
+        public string DisPlayName { get; set; }
+        public bool Emphasize { get; set; }
+        public string Required { get; set; }
+        public bool Checked { get; set; }
+        public string Description { get; set; }
+    }
+}
