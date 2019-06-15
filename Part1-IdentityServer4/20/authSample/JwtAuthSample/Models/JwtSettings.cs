e58ea9be8b6f91e9a1d685e1c56ec73b7e6902e9
mark <837216547@qq.com>
2019-06-15 16:19:33 +0800

    添加20节示例代码-Consent Controller Get请求逻辑实现

---
+++ Part1-IdentityServer4/20/authSample/JwtAuthSample/Models/JwtSettings.cs
@@ -0,0 +1,10 @@
+namespace JwtAuthSample
+{
+    public class JwtSettings
+    {
+        public string Issuer{get;set;}
+        public string Audience{get;set;}
+        public string SecretKey{get;set;}
+    }
+
+}
---
+++ Part1-IdentityServer4/20/authSample/mvcCookieAuthSample2/Config.cs
@@ -0,0 +1,91 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcCookieAuthSample
+{
+    public class Config
+    {
+        /// <summary>
+        /// 定义要保护的API资源，提供外部可访问的API资源
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1","api Application")
+            };
+        }
+
+        /// <summary>
+        /// 针对用户密码验证方式需要比对的账户
+        /// </summary>
+        /// <returns></returns>
+        public static List<TestUser> GetTestUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "10000",
+                    Username = "mark",
+                    Password = "password"
+                }
+            };
+        }
+
+        /// <summary>
+        /// 这个方法是来规范token生成的规则和方法的，（一般不进行设置，直接采用默认的即可，这里做了一下设置）
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
+            };
+        }
+
+        /// <summary>
+        /// 客户端注册，定义客户端允许访问的API资源（作用域名称和验证规则）
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client
+                {
+                    ClientId = "mvc",
+                    //授权方式为Implicit，类型为GrantType枚举
+                    AllowedGrantTypes ={ GrantType.Implicit },
+                    //授权密钥，客户端和服务端
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    //禁用权限确认
+                    //IdentityServer4 在登录完成的适合，会再跳转一次页面（权限确认）
+                    //但实际业务场景并不需要进行权限确认，而是登陆成功后直接跳转到之前的页面就行了
+                    RequireConsent = false,
+
+                    //oidc配置
+                    RedirectUris = { "http://localhost:5001/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:5001/signout-callback-oidc" },
+
+                    //允许客户端访问的Scopes[作用域]
+                    //AllowedScopes = { "api1" },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OpenId
+                    }
+                }
+            };
+        }
+    }
+}
