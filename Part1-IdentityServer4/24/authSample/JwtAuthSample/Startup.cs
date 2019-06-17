c3c7c89a96051b9e146b5c85f93234a9cb1032ce
mark <837216547@qq.com>
2019-06-17 14:02:30 +0800

    上传第24节代码-集成Asp.Net Identity

---
+++ Part1-IdentityServer4/24/authSample/JwtAuthSample/Startup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Swashbuckle.AspNetCore;
+using Swashbuckle.AspNetCore.Swagger;
+
+
+namespace JwtAuthSample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<JwtSettings>(Configuration.GetSection("JwtSettings"));
+
+            var jwtSettings = new JwtSettings();
+            Configuration.Bind("JwtSettings",jwtSettings);
+
+            services.AddAuthentication(options=>{
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(o=>{
+                //o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters{
+                //    ValidIssuer = jwtSettings.Issuer,
+                //    ValidAudience = jwtSettings.Audience,
+                //   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+                //};
+
+                o.SecurityTokenValidators.Clear();
+                o.SecurityTokenValidators.Add(new MyTokenValidator());
+
+                o.Events = new JwtBearerEvents(){
+                    OnMessageReceived = context =>{
+                        var token = context.Request.Headers["mytoken"];
+                        context.Token = token.FirstOrDefault();
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+            services.AddAuthorization(options=>{
+                options.AddPolicy("SuperAdminOnly", policy=> policy.RequireClaim("SuperAdminOnly"));
+            });
+
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1.1", new Info { Title = "My API", Version = "v1.1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
---
+++ Part1-IdentityServer4/24/authSample/mvcCookieAuthSample2/ViewModels/ProcessConsentProcess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcCookieAuthSample.ViewModels
+{
+    public class ProcessConsentProcess
+    {
+        public string ReturnUrl { get; set; }
+        public bool IsRedirect => !string.IsNullOrEmpty(this.ReturnUrl);
+        public ConsentViewModel ViewModel { get; set; }
+        public string ValidationError { get; set; }
+    }
+}
